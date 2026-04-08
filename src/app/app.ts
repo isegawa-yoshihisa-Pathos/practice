a3f285e0bb55b64fe5c9f49d515f21b579863166
Yoshihisa Isegawa <yoshihisa.isegawa@pathoslogos.co.jp>
2026-04-08 14:36:11 +0900

    changed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('angular-practice');
+  tasks = [
+    {title: '牛乳を買う', done: false, deadline: new Date('2026-04-09')},
+    {title: '可燃ゴミを出す', done: true, deadline: new Date('2026-04-01')},
+    {title: '銀行に行く', done: false, deadline: new Date('2026-04-02')},
+  ];
+
+  today: Date = new Date();
+
+  newTask = {
+    title: '',
+    deadline: this.today,
+  };
+
+  addTask() {
+    this.tasks.push({title: this.newTask.title, done: false, deadline: this.newTask.deadline});
+    this.newTask = {
+      title: '',
+      deadline: this.today,
+    };
+  }
+
+  isOverdue(task: any) {
+    return !task.done && task.deadline.getTime() < this.today.getTime();
+  }
 }
